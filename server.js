4419ddf5bf87a4d310fd2a5a2e25f42881ced74e
Denham Coote <denhamcoote@gmail.com>
2017-02-01 11:46:50 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine', 'hbs');
+
+//Logging
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) console.log('Unable to append to log');
+  });
+  next();
+});
+
+// Maintenance mode
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs')
+// });
+
+// Access to static files in /public
+app.use(express.static(__dirname+'/public'))
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req,res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome'
+  })
+});
+
+app.get('/about', (req,res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  })
+});
+
+app.get('/bad', (req,res) => {
+  res.send({
+    error: 'Unable to continue'
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
